51513243ab1fa357ce42521327ecd8aa60106728
yidanzhu10 <yzhu.uh@gmail.com>
2022-02-15 16:31:22 -0500

    adjust footer alignment

--- index.js
+++ index.js
@@ -1,100 +1,97 @@
-$('#firstheart').on('click', function(event) {
-	var element = $('#firstheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
+$('.heart').on('click', function(event) {
+	var element = $(this).css('color');
+  	if(element == '#FF0000'){
+
     	$(event.currentTarget).css('color', 'black');
   	} else{
   		$(event.currentTarget).css({color: 'red' });
   	}
-	
-	
-
 });
 
-$('#secondheart').on('click', function(event) {
-	var element = $('#secondheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#thirdheart').on('click', function(event) {
-	var element = $('#thirdheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#forthheart').on('click', function(event) {
-	var element = $('#forthheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#fifthheart').on('click', function(event) {
-	var element = $('#fifthheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#sixthheart').on('click', function(event) {
-	var element = $('#sixthheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#seventhheart').on('click', function(event) {
-	var element = $('#seventhheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#eighthheart').on('click', function(event) {
-	var element = $('#eighthheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
-
-$('#ninthheart').on('click', function(event) {
-	var element = $('#ninthheart').css('color');
-  	if(element == 'rgb(255, 0, 0)'){
-  	
-    	$(event.currentTarget).css('color', 'black');
-  	} else{
-  		$(event.currentTarget).css({color: 'red' });
-  	}
-	
-});
+// $('#secondheart').on('click', function(event) {
+// 	var element = $('#secondheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#thirdheart').on('click', function(event) {
+// 	var element = $('#thirdheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#forthheart').on('click', function(event) {
+// 	var element = $('#forthheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#fifthheart').on('click', function(event) {
+// 	var element = $('#fifthheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#sixthheart').on('click', function(event) {
+// 	var element = $('#sixthheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#seventhheart').on('click', function(event) {
+// 	var element = $('#seventhheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#eighthheart').on('click', function(event) {
+// 	var element = $('#eighthheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
+//
+// $('#ninthheart').on('click', function(event) {
+// 	var element = $('#ninthheart').css('color');
+//   	if(element == 'rgb(255, 0, 0)'){
+//
+//     	$(event.currentTarget).css('color', 'black');
+//   	} else{
+//   		$(event.currentTarget).css({color: 'red' });
+//   	}
+//
+// });
