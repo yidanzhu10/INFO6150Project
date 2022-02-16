50bd7ef267133669076979533ec8bd3750b55f8d
yidanzhu10 <yzhu.uh@gmail.com>
2022-02-16 13:57:39 -0500

    button removed

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ $('.heart').on('click', function(event) {
   		$(event.currentTarget).css('color', 'red');
   	}
 });
+
+// Product quantity add and minus
+
+$(.plus-btn).on('click', function(event) {
+	// event.preventDefault();
+	var $input = $(this).closest('div').find('input');
+	var value = parseInt($input.val());
+	if(value >= 0) {
+		value += 1;
+	} else {
+		value = 0;
+	}
+	$input.val(value);
+})
