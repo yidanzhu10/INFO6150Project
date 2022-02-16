6796f50d5ec034d0da6f6a44d8b8f632284a0ccf
Destination4298 <zhang.fengy@northeastern.edu>
2022-02-16 13:52:38 -0500

    change pages of nav-bar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // like function on the product
 $('.heart').on('click', function(event) {
 	var element = $(this).css('color');
-  	if(element == '#FF0000'){
+  	if(element == 'rgb(255, 0, 0)'){
     	$(event.currentTarget).css('color', 'black');
   	} else{
   		$(event.currentTarget).css('color', 'red');
